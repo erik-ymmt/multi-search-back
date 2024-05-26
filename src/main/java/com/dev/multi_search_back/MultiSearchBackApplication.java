f0bc943a40f12d0850da995f41213ab0878dccdb
Erik Yamamoto <erik.yamamoto@bringitps.com>
2024-05-26 14:40:50 -0300

    First commit - Spring base project

---
+++ src/main/java/com/dev/multi_search_back/MultiSearchBackApplication.java
@@ -0,0 +1,15 @@
+package com.dev.multi_search_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class MultiSearchBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSearchBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dev/multi_search_back/MultiSearchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.multi_search_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSearchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
