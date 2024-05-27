20ca08ced754768226989e01401959b4b9f72e5d
Erik Yamamoto <erik.yamamoto@bringitps.com>
2024-05-27 16:07:29 -0300

    Add cross origin for deploy

--- src/main/java/com/dev/multi_search_back/MultiSearchBackApplication.java
+++ src/main/java/com/dev/multi_search_back/MultiSearchBackApplication.java
@@ -2,10 +2,11 @@ package com.dev.multi_search_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@CrossOrigin
 @RestController
 
 public class MultiSearchBackApplication {
